5288b1051388c8d5a9a4155c2d0be3d990c2f953
aniekeme <mercysam299@gmail.com>
2023-02-03 04:34:04 +0100

    added host-inventory and outputs

--- LbOutputs.tf
+++ LbOutputs.tf
@@ -3,3 +3,16 @@ output "vpc_id" {
   description = "ID of project VPC"
   value       = aws_vpc.Altschool-project-vpc.id
 }
+
+output "public_ip1" {
+  value       = aws_instance.Altschool-1.public_ip
+  description = "my 1st server public ip"
+}
+output "public_ip2" {
+  value       = aws_instance.Altschool-2.public_ip
+  description = "my 2nd server public ip"
+}
+output "public_ip3" {
+  value       = aws_instance.Altschool-3.public_ip
+  description = "my 3rd server public ip"
+}
