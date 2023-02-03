148aa539087419f45744dcc61254e1f767c33b2a
aniekeme <mercysam299@gmail.com>
2023-02-03 04:50:17 +0100

    corrected ip address

--- LbOutputs.tf
+++ LbOutputs.tf
@@ -5,14 +5,14 @@ output "vpc_id" {
 }
 
 output "public_ip1" {
-  value       = aws_instance.Altschool-1.public_ip
+  value       = aws_instance.Altschool1.public_ip
   description = "my 1st server public ip"
 }
 output "public_ip2" {
-  value       = aws_instance.Altschool-2.public_ip
+  value       = aws_instance.Altschool2.public_ip
   description = "my 2nd server public ip"
 }
 output "public_ip3" {
-  value       = aws_instance.Altschool-3.public_ip
+  value       = aws_instance.Altschool3.public_ip
   description = "my 3rd server public ip"
 }
